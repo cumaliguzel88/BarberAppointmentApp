1f8d7ac6986cde1384da12175db35cc18851ae7e
CUMALİ GÜZEL <cumali.guzel@std.yeditepe.edu.tr>
2025-03-18 19:36:27 +0300

    Berber 1.0 düzenleme yaptım

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.cumaliguzel.barberappointment"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.cumaliguzel.barberappointment"
